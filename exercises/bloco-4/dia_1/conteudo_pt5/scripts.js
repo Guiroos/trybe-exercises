f5974fa8a1d6fe98d113cf0a56535a281c65605f
guiroos <guilherme.roosr@gmail.com>
2021-06-23 16:03:31 -0300

    Adicionado o resto do conteudo_pt5

---
+++ exercises/bloco-4/dia_1/conteudo_pt5/scripts.js
@@ -0,0 +1,22 @@
+//switch - case
+
+let trafficLight = "sim";
+
+switch (trafficLight) {
+  case "vermelho":
+    console.log("Pare!");
+    break
+ 
+  case "amarelo":
+    console.log("Atenção");
+    break
+
+  case "verde":
+    console.log("Siga");
+    break
+  
+  default:
+    console.log("Valor não identificado");  
+}
+
+//exemplo video
