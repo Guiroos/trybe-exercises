d64e27853893525968d8c814d56a154913a05cdd
guiroos <guilherme.roosr@gmail.com>
2021-06-24 15:48:20 -0300

    Adicionado exercicios 1 ao 9

---
+++ exercises/bloco-4/dia_2/exe/exe9.js
@@ -0,0 +1,8 @@
+let numeroIncial = 1;
+let numeroFinal = 25;
+let numeros = [];
+
+for (let index = numeroIncial; index <= numeroFinal; index += 1) {
+  numeros.push(index/2);
+}
+console.log(numeros);
