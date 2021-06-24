878abe7ae8c43384ccb0dc84e92266d50c57135c
guiroos <guilherme.roosr@gmail.com>
2021-06-24 01:27:28 -0300

    Arrumado 8 e 9 e adicionado 10 e 11

--- exercises/bloco-4/dia_1/exe/scripts.js
+++ exercises/bloco-4/dia_1/exe/scripts.js
@@ -1,5 +1,7 @@
 // Exercicio 1
 
+console.log("Exercicio 1");
+
 let a = 5;
 let b = 2;
 
@@ -18,6 +20,8 @@ console.log();
 
 // Exercicio 2
 
+console.log("Exercicio 2");
+
 let valor1 = 55;
 let valor2 = 51;
 
@@ -30,6 +34,8 @@ else {
 console.log();
 
 // Exercicio 3
+
+console.log("Exercicio 3");
 
 let valor3 = 100;
 let valor4 = 84;
@@ -48,6 +54,8 @@ console.log();
 
 // Exercicio 4
 
+console.log("Exercicio 4");
+
 let pnz = 0;
 
 if (pnz > 0) {
@@ -63,6 +71,8 @@ console.log();
 
 // Exercicio 5
 
+console.log("Exercicio 5");
+
 let angulo_1 = 50;
 let angulo_2 = 100;
 let angulo_3 = 30;
@@ -77,6 +87,8 @@ else {
 console.log();
 
 // Exercicio 6
+
+console.log("Exercicio 6");
 
 let pecaXadrez = "rambo";
 let pecaXadrezLc = pecaXadrez.toLocaleLowerCase(); 
@@ -114,6 +126,8 @@ console.log();
 
 // Exercicio 7
 
+console.log("Exercicio 7");
+
 let notaPercentage = 100;
  
 if (notaPercentage >= 90 && notaPercentage <= 100) {
@@ -141,30 +155,94 @@ console.log();
 
 // Exercicio 8
 
+console.log("Exercicio 8");
+
 let numEven1 = 1;
-let numEven2 = 13;
+let numEven2 = 14;
 let numEven3 = 27;
 
+let isEven = false;
+
 if (numEven1%2 == 0 || numEven2%2 == 0 || numEven3%2 == 0) {
-  console.log("True")
+  isEven = true;
 }
-else {
-  console.log("False");
-}
+
+console.log(isEven);
 console.log();
 
 // Exercicio 9
 
-let numOdd1 = 2;
+console.log("Exercicio 9");
+
+let numOdd1 = 3;
 let numOdd2 = 14;
 let numOdd3 = 28;
 
+let isOdd = false;
+
 if (numOdd1%2 != 0 || numOdd2%2 != 0 || numOdd3%2 != 0) {
-  console.log("True")
+  isOdd = true;
 }
-else {
-  console.log("False");
-}
+
+console.log(isOdd);
 console.log();
 
 // Exercicio 10
+
+console.log("Exercicio 10");
+
+let custoProduto = 2;
+let vendaProduto = 5;
+
+if ( custoProduto >= 0 && vendaProduto >= 0) {
+  let custoTotalProduto = custoProduto * 1.2;
+  let lucroMil = (vendaProduto - custoTotalProduto) * 1000;
+  console.log(lucroMil)
+}
+else {
+  console.log("Error, os valores não podem ser negativos");
+}
+
+console.log();
+
+// Exercicio 11
+
+console.log("Exercicio 11");
+
+let salarioBruto = 3000;
+let salarioBase;
+let salarioLiquido;
+
+if (salarioBruto <= 1556.94) {
+  salarioBase = salarioBruto * 0.92;
+}
+else if (salarioBruto > 1556.95 && salarioBruto < 2594.92) {
+  salarioBase = salarioBruto * 0.91;
+}
+else if (salarioBruto > 2594.93 && salarioBruto < 5189.82) {
+  salarioBase = salarioBruto * 0.89
+}
+else {
+  salarioBase = salarioBruto - 570.88;
+}
+
+if (salarioBase <= 1903.98) {
+  salarioLiquido = salarioBase - 0;
+  console.log("Seu salário líquido será " + "R$" + salarioLiquido);
+}
+else if (salarioBase > 1903.99 && salarioBase < 2826.65) {
+  salarioLiquido = (salarioBase * 0.925) + 142.8;
+  console.log("Seu salário líquido será " + "R$" + salarioLiquido);
+}
+else if (salarioBase > 2826.66 && salarioBase < 3751,05) {
+  salarioLiquido = (salarioBase * 0.85) + 354.8;
+  console.log("Seu salário líquido será " + "R$" + salarioLiquido);
+}
+else if (salarioBase > 3751,06 && salarioBase < 4664.68) {
+  salarioLiquido = (salarioBase * 0.775) + 636.13;
+  console.log("Seu salário líquido será " + "R$" + salarioLiquido);
+}
+else {
+  salarioLiquido = (salarioBase * 0,725) + 869.36;
+  console.log("Seu salário líquido será " + "R$" + salarioLiquido);
+}
