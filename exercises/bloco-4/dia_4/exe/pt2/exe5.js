f3da8a0dfcb9dec8bec7cd7721541bac2ebcd55e
guiroos <guilherme.roosr@gmail.com>
2021-06-29 13:29:44 -0300

    Terminado exercicio 5 e adicionado o 6 e 7

--- exercises/bloco-4/dia_4/exe/pt2/exe5.js
+++ exercises/bloco-4/dia_4/exe/pt2/exe5.js
@@ -1,18 +1,23 @@
 //Exercicio 5
 
-function repeatingNumbers (numbers) {
+function repeatingNumbers(numbers) {
   let addNumber = 0;
+  let repeater = 0;
+  let indexRepeatingNumber = 0;
   for(let index in numbers) {
+    let checkNumber = numbers[index];
     for (let index2 in numbers) {
-      let checkNumber = numbers[index];
       if (checkNumber === numbers[index2]) {
         addNumber += 1;
       }
-
     }
-    if (addNumber > )
+    if (addNumber > repeater) {
+      repeater = addNumber;
+      indexRepeatingNumber = index;
+    }
+    addNumber = 0;
   }
-
+  return numbers[indexRepeatingNumber];
 }
 
 console.log(repeatingNumbers([2, 3, 2, 5, 8, 2, 3]));
