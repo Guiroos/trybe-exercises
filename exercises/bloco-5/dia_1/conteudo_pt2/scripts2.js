8a6a2b07c6d785e078e3e7c412e5b24bf977ef08
guiroos <guilherme.roosr@gmail.com>
2021-07-05 14:25:38 -0300

    Adicionado bloco 5 e conteudo pt2 do dia 1

---
+++ exercises/bloco-5/dia_1/conteudo_pt2/scripts2.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByClassName("paragrafos"));
+let classParagraph = document.getElementsByClassName("paragrafos");
+classParagraph[0].style.fontSize = "20px";
+classParagraph[1].style.fontSize = "30px";
+
+let h4 = document.getElementsByTagName("h4");
+h4[0].style.color = "red";
