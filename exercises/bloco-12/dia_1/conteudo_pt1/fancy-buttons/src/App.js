fc137c6f24e4a5aafe86ce2090b9294125f12973
guiroos <guilherme.roosr@gmail.com>
2021-09-20 06:12:47 -0300

    Fixacao 5, 6 e 7

--- exercises/bloco-12/dia_1/conteudo_pt1/fancy-buttons/src/App.js
+++ exercises/bloco-12/dia_1/conteudo_pt1/fancy-buttons/src/App.js
@@ -8,24 +8,38 @@ class App extends React.Component {
     this.handleClickOne = this.handleClickOne.bind(this);
     this.handleClickTwo = this.handleClickTwo.bind(this);
     this.handleClickThree = this.handleClickThree.bind(this);
+    this.state = {
+      clicksOne: 0,
+      clicksTwo: 0,
+      clicksThree: 0.
+    }
   }
 
-  handleClickOne () {
+  handleClickOne() {
+    this.setState((estadoAnterior, _props) => ({
+      clicksOne: estadoAnterior.clicksOne + 1
+    }))
     console.log("'this' do botão 1", this);
   }
-  handleClickTwo () {
+  handleClickTwo() {
+    this.setState((estadoAnterior, _props) => ({
+      clicksTwo: estadoAnterior.clicksTwo + 1
+    }))
     console.log("'this' do botão 2", this);
   }
-  handleClickThree () {
+  handleClickThree() {
+    this.setState((estadoAnterior, _props) => ({
+      clicksThree: estadoAnterior.clicksThree + 1
+    }))
     console.log("'this' do botão 3", this);
   }
 
   render () {
     return (
       <div>
-        <button onClick={this.handleClickOne}>Botão 1</button>
-        <button onClick={this.handleClickTwo}>Botão 2</button>
-        <button onClick={this.handleClickThree}>Botão 3</button>
+        <button onClick={this.handleClickOne}>{this.state.clicksOne}</button>
+        <button onClick={this.handleClickTwo}>{this.state.clicksTwo}</button>
+        <button onClick={this.handleClickThree}>{this.state.clicksThree}</button>
       </div>
     )
   }
