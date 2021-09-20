16064b5930be27961b6fceed41e69d95ea808270
guiroos <guilherme.roosr@gmail.com>
2021-09-20 01:04:33 -0300

    Adicionado conteudo_pt2

---
+++ exercises/bloco-11/dia_1/conteudo_pt2/exemplo-2/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Guilherme Roos</h1>
+        <p>Tenho 24 anos e sou engenheiro mecânico.</p>
+      </div>
+    );
+  }
+}
+
+export default Component;
