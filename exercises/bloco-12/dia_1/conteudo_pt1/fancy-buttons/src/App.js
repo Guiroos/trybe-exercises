ad75ae2f53540c411e62554e3028a08ae22edfb5
guiroos <guilherme.roosr@gmail.com>
2021-09-20 06:24:02 -0300

    Fixacao 8 e 9

--- exercises/bloco-12/dia_1/conteudo_pt1/fancy-buttons/src/App.js
+++ exercises/bloco-12/dia_1/conteudo_pt1/fancy-buttons/src/App.js
@@ -18,28 +18,39 @@ class App extends React.Component {
   handleClickOne() {
     this.setState((estadoAnterior, _props) => ({
       clicksOne: estadoAnterior.clicksOne + 1
-    }))
+    }), () => {
+      console.log(`Botão 1 ${this.buttonColor(this.state.clicksOne)}`);
+    });
     console.log("'this' do botão 1", this);
   }
   handleClickTwo() {
     this.setState((estadoAnterior, _props) => ({
       clicksTwo: estadoAnterior.clicksTwo + 1
-    }))
+    }), () => {
+      console.log(`Botão 2 ${this.buttonColor(this.state.clicksTwo)}`);
+    });
     console.log("'this' do botão 2", this);
   }
   handleClickThree() {
     this.setState((estadoAnterior, _props) => ({
       clicksThree: estadoAnterior.clicksThree + 1
-    }))
+    }), () => {
+      console.log(`Botão 3 ${this.buttonColor(this.state.clicksThree)}`);
+    });
     console.log("'this' do botão 3", this);
   }
 
+  buttonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   render () {
+    const { clicksOne, clicksTwo, clicksThree} = this.state;
     return (
       <div>
-        <button onClick={this.handleClickOne}>{this.state.clicksOne}</button>
-        <button onClick={this.handleClickTwo}>{this.state.clicksTwo}</button>
-        <button onClick={this.handleClickThree}>{this.state.clicksThree}</button>
+        <button style={{ backgroundColor: this.buttonColor(clicksOne) }} onClick={this.handleClickOne}>{this.state.clicksOne}</button>
+        <button style={{ backgroundColor: this.buttonColor(clicksTwo) }} onClick={this.handleClickTwo}>{this.state.clicksTwo}</button>
+        <button style={{ backgroundColor: this.buttonColor(clicksThree) }} onClick={this.handleClickThree}>{this.state.clicksThree}</button>
       </div>
     )
   }
