c7e4a1ef009c2b89d77cb2da8965077e0b7e0d38
guiroos <guilherme.roosr@gmail.com>
2021-09-20 05:55:21 -0300

    Fixacao 3 e 4

--- exercises/bloco-12/dia_1/conteudo_pt1/fancy-buttons/src/App.js
+++ exercises/bloco-12/dia_1/conteudo_pt1/fancy-buttons/src/App.js
@@ -1,17 +1,31 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 
-const handleClickOne = () => console.log("Você clicou!")
-const handleClickTwo = () => console.log("Esse botão não faz nada de especial...")
-const handleClickThree = () => console.log("Surpresa?")
 
 class App extends React.Component {
+  constructor () {
+    super();
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+  }
+
+  handleClickOne () {
+    console.log("'this' do botão 1", this);
+  }
+  handleClickTwo () {
+    console.log("'this' do botão 2", this);
+  }
+  handleClickThree () {
+    console.log("'this' do botão 3", this);
+  }
+
   render () {
     return (
       <div>
-        <button onClick={handleClickOne}>Botão 1</button>
-        <button onClick={handleClickTwo}>Botão 2</button>
-        <button onClick={handleClickThree}>Botão 3</button>
+        <button onClick={this.handleClickOne}>Botão 1</button>
+        <button onClick={this.handleClickTwo}>Botão 2</button>
+        <button onClick={this.handleClickThree}>Botão 3</button>
       </div>
     )
   }
