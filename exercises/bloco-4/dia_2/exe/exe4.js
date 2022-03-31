f11df24623d3c42a34ce759901558cf232e163ea
guiroos <guilherme.roosr@gmail.com>
2022-03-31 09:05:09 -0300

    refactoring bloco-4, dia_2, exercises to for-of

--- exercises/bloco-4/dia_2/exe/exe4.js
+++ exercises/bloco-4/dia_2/exe/exe4.js
@@ -1,8 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaNumber = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
+/* for (let index = 0; index < numbers.length; index += 1) {
   somaNumber = somaNumber + numbers[index];
+} */
+
+for (let number of numbers) {
+  somaNumber += number;
 }
 
 let mediaNumber = somaNumber / numbers.length;
--- exercises/bloco-4/dia_2/exe/exe5.js
+++ exercises/bloco-4/dia_2/exe/exe5.js
@@ -1,10 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorNumero = numbers[0];
 
-for (let index = 0; index < numbers.length; index += 1) {
-
+/* for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] > maiorNumero) {
     maiorNumero = numbers[index];
   }
+} */
+
+for (let number of numbers) {
+  if (number > maiorNumero) {
+    maiorNumero = number;
+  }
 }
+
 console.log(maiorNumero);
